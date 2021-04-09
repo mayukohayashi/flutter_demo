1941694cbc849521dfbe2c01fd0c610f11114033
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2021-04-09 10:24:30 +0900

    refac: refacctoring

--- lib/result.dart
+++ lib/result.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     var resultText = "You did IT!";
@@ -23,10 +24,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      resultPhrase,
-      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-      textAlign: TextAlign.center,
+        child: Column(
+      children: <Widget>[
+        Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        TextButton(
+          child: Text("Restart Quiz!"),
+          onPressed: resetHandler,
+        ),
+      ],
     ));
   }
 }
